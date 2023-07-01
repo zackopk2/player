38500ca59b10ecd9c1bf5e772f141d2f73fffe55
zackopk2 <90936576+zackopk2@users.noreply.github.com>
2023-06-30 22:27:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,6 @@ function playVideo() {
   // Set the Referer header for the video request
   var xhr = new XMLHttpRequest();
   xhr.open("GET", videoUrl, true);
-  xhr.setRequestHeader("Referer", "78373fa444d18cf962d99179d6621a64");
+  xhr.setRequestHeader("Referer", "/referer=78373fa444d18cf962d99179d6621a64");
   xhr.send();
 }
