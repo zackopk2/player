bfc2edad201dddca18e687376422157f118511dd
zackopk2 <90936576+zackopk2@users.noreply.github.com>
2023-06-30 22:21:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function playVideo() {
+  var videoUrl = document.getElementById("videoInput").value;
+  var videoPlayer = document.getElementById("videoPlayer");
+
+  var source = document.createElement("source");
+  source.setAttribute("src", videoUrl);
+  videoPlayer.innerHTML = "";
+  videoPlayer.appendChild(source);
+
+  // Set the Referer header for the video request
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", videoUrl, true);
+  xhr.setRequestHeader("Referer", "78373fa444d18cf962d99179d6621a64");
+  xhr.send();
+}
